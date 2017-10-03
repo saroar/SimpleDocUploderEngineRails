b58b1e679e2956f58e54df6f92585205cdbf68ff
saroar <saroar9@gmail.com>
2017-10-03 23:36:31 +0300

    Configure for Rspec and Factory Girl

--- lib/simpledocupload/engine.rb
+++ lib/simpledocupload/engine.rb
@@ -1,5 +1,17 @@
+# require dependencies declared in your gemspec
+Gem.loaded_specs['simpledocupload'].dependencies.each do |d|
+ require d.name
+end
+
 module Simpledocupload
   class Engine < ::Rails::Engine
+
     isolate_namespace Simpledocupload
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
+
   end
 end
